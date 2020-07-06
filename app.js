cee340c8ea7f121af097ff960f79cff33e298ec2
Anmol Pant <anmolpant@gmail.com>
2020-07-06 16:13:22 +0530

    change sound and vid

--- app.js
+++ app.js
@@ -21,6 +21,17 @@ const app = () => {
     outline.style.strokeDasharray = outlineLength;
     outline.style.strokeDashoffset = outlineLength;
 
+    //Pick different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click',function(){
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+    });
+
+
+
 //play sound
     play.addEventListener('click', ()=>{
         checkPlaying(song);
